9bb4bb36ef03bfede719192e064ac4584398d541
frankw <fwessels@xs4all.nl>
2017-09-01 11:58:55 -0700

    Refactored accessing key header structure

--- key.go
+++ key.go
@@ -2,6 +2,7 @@ package kvbos
 
 import (
 	"encoding/binary"
+	_ "bytes"
 )
 
 // TODO: Add size of block
@@ -19,14 +20,14 @@ import (
 //	Keys           []Key /* stored from the top down */
 //}
 
-//type Key struct {
+//type Key(Header) struct {
 //	valuePointer uint64
 //	valueSize	 uint32
 //	keySize 	 uint32
 //	key		     []byte
 //}
 
-// Try to read it again
+// TODO: Try to read it again when mismatch detected while reading merged blocks
 //
 const KeyHeaderSize = 8 + 4 + 4
 
@@ -34,14 +35,46 @@ type KeyHeader []byte
 
 func newKeyHeader(b []byte, size uint32) KeyHeader { return KeyHeader(b[:size]) }
 
-func (k KeyHeader) ValuePointer() uint64 { return uint64(binary.LittleEndian.Uint64(k[0:])) }
+func (kh KeyHeader) ValuePointer() uint64 { return uint64(binary.LittleEndian.Uint64(kh[0:])) }
 
-func (k KeyHeader) ValueSize() uint32 { return uint32(binary.LittleEndian.Uint32(k[8:])) }
+func (kh KeyHeader) ValueSize() uint32 { return uint32(binary.LittleEndian.Uint32(kh[8:])) }
 
-func (k KeyHeader) KeySize() uint32 { return uint32(binary.LittleEndian.Uint32(k[12:])) }
+func (kh KeyHeader) KeySize() uint32 { return uint32(binary.LittleEndian.Uint32(kh[12:])) }
 
-func (k KeyHeader) SetValuePointer(vp uint64) { binary.LittleEndian.PutUint64(k[0:], vp) }
+func (kh KeyHeader) KeyAlignedSize() uint64 { return uint64((kh.KeySize() + KeyAlign-1) & ^uint32(KeyAlign-1)) }
 
-func (k KeyHeader) SetValueSize(vs uint32) { binary.LittleEndian.PutUint32(k[8:], vs) }
+func (kh KeyHeader) SetValuePointer(vp uint64) { binary.LittleEndian.PutUint64(kh[0:], vp) }
 
-func (k KeyHeader) SetKeySize(ks uint32) { binary.LittleEndian.PutUint32(k[12:], ks) }
+func (kh KeyHeader) SetValueSize(vs uint32) { binary.LittleEndian.PutUint32(kh[8:], vs) }
+
+func (kh KeyHeader) SetKeySize(ks uint32) { binary.LittleEndian.PutUint32(kh[12:], ks) }
+
+//type KeyBlockHeader struct {
+//	entries uint32
+//	unused uint32
+//  sortedKeyPointers []uint64
+//}
+
+type KeyBlockHeader []byte
+
+func newKeyBlockHeader(b []byte) KeyBlockHeader { return KeyBlockHeader(b) }
+
+func (kbh KeyBlockHeader) Entries() uint32 { return uint32(binary.LittleEndian.Uint32(kbh[0:])) }
+
+func (kbh KeyBlockHeader) AddSortedPointer(keyPointer uint64) {
+
+	entries := kbh.Entries()
+	if entries == 0 {
+		binary.LittleEndian.PutUint64(kbh[8:], keyPointer)
+	} else {
+		//cmp = Compare(binary.LittleEndian.Uint64(kbh[8:], keyPointer)
+	}
+
+	binary.LittleEndian.PutUint32(kbh[0:], entries + 1)
+}
+
+//func Compare(a, b uint64) int {
+//	akh := newKeyHeader(KeyBlocks[0][a:], KeyHeaderSize)
+//	bkh := newKeyHeader(KeyBlocks[0][b:], KeyHeaderSize)
+//	return bytes.Compare()
+//}
--- kvbos.go
+++ kvbos.go
@@ -42,14 +42,19 @@ func (kvb *KVBos) Put(key []byte, value []byte) {
 	kh.SetValueSize(valueSize)
 	kh.SetKeySize(keySize)
 
-	// Copy key header
+	// Copy key header first (has a deterministic size,
+	// so we can iterate manually 'downwards' in memory if need be)
 	copy(KeyBlocks[0][KeyPointer-uint64(KeyHeaderSize-1):], kh[:])
 
 	// Copy key itself
-	keyAlignedSize := (keySize + KeyAlign-1) & ^uint32(KeyAlign-1)
+	keyAlignedSize := kh.KeyAlignedSize()
 	copy(KeyBlocks[0][KeyPointer-uint64(KeyHeaderSize+keyAlignedSize-1):], key[:])
 
 	KeyPointer -= KeyHeaderSize + uint64(keyAlignedSize)
+
+	kbh := newKeyBlockHeader(KeyBlocks[0][:])
+	fmt.Println(kbh.Entries())
+	kbh.AddSortedPointer(KeyPointer + 1 + keyAlignedSize)
 }
 
 func (kvb *KVBos) Get(key []byte) []byte {
@@ -57,22 +62,19 @@ func (kvb *KVBos) Get(key []byte) []byte {
 	keyPointer := uint64(0x7f)
 
 	for i := 0; i < 3; i++ {
-		kh := newKeyHeader(KeyBlocks[0][keyPointer-uint64(KeyHeaderSize-1):], KeyHeaderSize)
+		pKeyHdr := keyPointer-(KeyHeaderSize-1)
+		kh := newKeyHeader(KeyBlocks[0][pKeyHdr:], KeyHeaderSize)
 
-		keyAlignedSize := (kh.KeySize() + KeyAlign-1) & ^uint32(KeyAlign-1)
-
-		k := make([]byte, kh.KeySize())
-		copy(k, KeyBlocks[0][keyPointer-uint64(KeyHeaderSize+keyAlignedSize-1):keyPointer-uint64(KeyHeaderSize+keyAlignedSize-1-kh.KeySize())])
-
-		if bytes.Compare(k, key) == 0 {
-			fmt.Println("k-e-y   f-o-u-n-d =", string(k))
+		pKeyData := pKeyHdr-kh.KeyAlignedSize()
+		if bytes.Compare(/*k*/KeyBlocks[0][pKeyData:pKeyData+uint64(kh.KeySize())], key) == 0 {
+			fmt.Println("kh-e-y   f-o-u-n-d =", string(KeyBlocks[0][pKeyData:pKeyData+uint64(kh.KeySize())]))
 
 			v := make([]byte, kh.ValueSize())
 			copy(v, ValueBlocks[0][kh.ValuePointer():kh.ValuePointer()+uint64(kh.ValueSize())])
 			return v
 		}
 
-		keyPointer -= KeyHeaderSize + uint64(keyAlignedSize)
+		keyPointer -= KeyHeaderSize + uint64(kh.KeyAlignedSize())
 	}
 
 	return []byte{}
