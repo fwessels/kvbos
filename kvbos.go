fec71b29b90c8c030893c1f0fa2eb873b34bc043
frankw <fwessels@xs4all.nl>
2017-09-01 15:04:18 -0700

    Skip first 0x10 bytes

--- kvbos.go
+++ kvbos.go
@@ -1,7 +1,5 @@
 package kvbos
 
-import ()
-
 type Value struct {
 	value []byte
 }
@@ -13,7 +11,8 @@ const (
 	ValueBlockShift = 5
 	ValueBlockSize  = 1 << ValueBlockShift
 	ValueBlockMask  = ValueBlockSize - 1
-	KeyBlockSize    = 256
+	KeyBlockShift   = 8
+	KeyBlockSize    = 1 << KeyBlockShift
 	KeyBlockMask    = KeyBlockSize - 1
 	KeyAlign        = 8
 )
@@ -22,10 +21,10 @@ type ValueBlock [ValueBlockSize]byte
 type KeyBlock [KeyBlockSize]byte
 
 var ValueBlocks [10]ValueBlock
-var ValuePointer = uint64(0x0000000000000000)
+var ValuePointer = uint64(0x0000000000000010) // Skip first 0x10 bytes (prevent 'NULL' from being a valid value pointer)
 
 var KeyBlocks [10]KeyBlock
-var KeyPointer = uint64(0xffffffffffffffff) // uint64(KeyBlockSize-1)
+var KeyPointer = uint64(0xffffffffffffffff)
 
 func (kvb *KVBos) Put(key []byte, value []byte) {
 
@@ -34,9 +33,11 @@ func (kvb *KVBos) Put(key []byte, value []byte) {
 	valueSize := uint32(len(value))
 	keySize := uint32(len(key))
 	if valueSize >= ValueBlockSize {
+		// TODO: Could "overflow" value into a new block (provided it is smaller than uint32)
+		// and would require multiple blocks to read (or multiple HTTP Range GETs)
 		panic("Attempting to store item larger than value block size")
 	} else if (valuePointer&ValueBlockMask)+uint64(valueSize) >= ValueBlockSize {
-		valuePointer = ((valuePointer >> ValueBlockShift) + 1) << ValueBlockShift
+		valuePointer = ((valuePointer >> ValueBlockShift) + 1) << ValueBlockShift // advance to beginning of next block
 	}
 	copy(ValueBlocks[valuePointer>>ValueBlockShift][valuePointer&ValueBlockMask:], value[:])
 
